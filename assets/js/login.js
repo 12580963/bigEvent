4f6312f5b84eba81cd04f7559248fa6148720e7e
cff <2935505626@qq.com>
2020-10-12 10:36:44 +0800

    提交

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    var form = layui.form;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    })
+})
